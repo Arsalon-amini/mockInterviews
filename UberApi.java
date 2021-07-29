a1861ddee014d0fdcef074d77a1000807e4543fa
arsalon <arsalonamini2020@gmail.com>
2021-07-29 13:43:11 +0800

    Design Uber API - mock interview question for system design API design

---
+++ UberApi.java
@@ -0,0 +1,113 @@
+/*
+Prompt: Design Uber API 
+
+Entities: 
+
+Ride : { 
+    id: Integer,
+    passengerId: Integer,
+    driverId: Integer,
+    rideStatusId: Integer
+    requestTime: timeStamp,
+    pickup: Location,
+    dropOff: Location,
+    price: Integer
+}
+
+passenger: {
+    id: Integer,
+    name: string,
+    rating: string
+}
+
+driver: {
+    id: Integer,
+    name: string,
+    fiveStarRatings: Integer,
+    ridesCompleted: Integer,
+    rating: Integer,
+    status: Enum [available, busy, availableSoon]
+}
+
+rideStatus: {
+    id: Integer,
+    createdAt: timeStamp,
+    matched: boolean,
+    inProgress: boolean,
+    cancelled: boolean (default=false),
+    completed: boolean
+}
+
+** Passenger API ***
+
+POST Request: post a ride 
+Input: passenger entity, Ride entity 
+Output: Ride entity 
+
+Java
+public static Ride requestRide(Passenger passenger, Ride ride){
+    //validate args, return any errors
+    //store Ride entity in database
+    //call a helper method for matching passenger with ride (async operation)
+    //return a Ride object w/ driverId
+}
+
+JS
+const Ride = async (req, res) => {}
+
+GET: receive data on ride 
+Input: ride_.id
+Output: RideStatus entity
+
+public static RideStatus getRideDetails(Integer ride_.id){
+    //query the database asynchronously until RideStatus entity is available
+    //return the RideStatus entity
+}
+
+DELETE: cancel a ride
+Input: ride_.id
+Output: Ride entity 
+
+public static Ride cancelRide (Integer ride_.id){
+    //helper method async calls driverAPI to inform driver on ride cancellation
+    //updates RideStatus entity in DB and ride object
+    //returns Ride w/ cancelled status 
+}
+
+PUT: update Ride desitionation
+Input: ride_.id
+Output: Ride entity 
+
+public static Ride updateRide(Integer ride_.id, Location newDestination){
+    //call db to set Ride entity location = newDestination
+    //return a Ride entity with updated location
+}
+
+*** Driver API ****
+
+GET: get a new ride
+Input:
+Output: Ride entity 
+
+public static Ride getNewRide(){
+    //query DB table of queued rides
+    //calls helper method for confirming the driver
+    //returns Ride entity
+}
+
+Helper Method: confirm driver, update Ride 
+
+public static void confirmRide(Integer ride_.id, Integer driver_id){
+    //requests the drivers confirmation for taking the ride
+    //if yes, updates the Ride entity with driverId
+    //if no, puts the Ride back into the SQL table for queued rides needing drivers
+}
+
+DELETE: cancel a ride by driver
+public static Ride cancelRide(Integer ride_.id){
+    //gets the Ride entity in DB 
+    //updates the RideStatus to Cancelled, restores in DB
+    //sends a notification to the user that ride is cancelled
+    //puts ride_.id back into the queue for matching with an available driver
+}
+/*
